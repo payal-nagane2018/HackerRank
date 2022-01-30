b015c62c4c5040e49e1ed93d38197ebea5674c30
Payal Nagane <97519168+payal-nagane2018@users.noreply.github.com>
2022-01-30 16:27:08 +0530

    Update Solution.java

--- Java/Introducation/Java Output Formatting/Solution.java
+++ Java/Introducation/Java Output Formatting/Solution.java
@@ -1,7 +1,3 @@
-Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test your understanding of formatting output using printf.
-
-To get you started, a portion of the solution is provided for you in the editor; you must format and print the input to complete the solution.
-
 Input Format
 
 Every line of input will contain a String followed by an integer.
